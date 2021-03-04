3720162123a4ac04e590fa91ccd328606f29f03a
huyhq <huyhq@coccoc.com>
2021-03-04 18:02:15 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 module.exports = {
-
   // This option controls if and how source maps are generated.
   // https://webpack.js.org/configuration/devtool/
-  devtool: 'eval-cheap-module-source-map',
+  devtool: "eval-cheap-module-source-map",
 
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
-    index: './src/index.js'
+    index: "./src/index.js",
   },
   // output: {
   //   path: path.resolve(__dirname, 'dist'),
@@ -18,7 +18,7 @@ module.exports = {
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
     port: 8080,
-    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    writeToDisk: false, // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
   },
 
   // https://webpack.js.org/concepts/loaders/
@@ -27,46 +27,50 @@ module.exports = {
       {
         test: /\.js$/i,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env']
-        }
+          presets: ["@babel/preset-env"],
+        },
       },
       {
         test: /\.(s*)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         // Load all images as base64 encoding if they are smaller than 8192 bytes
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               // On development we want to see where the file is coming from, hence we preserve the [path]
-              name: '[path][name].[ext]?hash=[hash:20]',
+              name: "[path][name].[ext]?hash=[hash:20]",
               esModule: false,
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ]
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
   },
 
   // https://webpack.js.org/concepts/plugins/
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       inject: true,
-      chunks: ['index'],
-      filename: 'index.html'
+      chunks: ["index"],
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-      template: './src/about.html',
+      template: "./src/about.html",
       inject: true,
-      chunks: ['index'],
-      filename: 'about.html'
+      chunks: ["index"],
+      filename: "about.html",
     }),
-  ]
-}
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+};
