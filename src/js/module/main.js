942f5130187bdc697a4b75f1ed4a0901ad3b1283
huyhq <huyhq@coccoc.com>
2021-04-05 23:54:23 +0700

    update

--- src/js/module/main.js
+++ src/js/module/main.js
@@ -280,19 +280,9 @@ $(".corevalue-slider").slick({
     {
       breakpoint: 767,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
+        unslick: true
       },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        autoplay: true,
-        autoplaySpeed: 2000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
+    }
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
@@ -317,7 +307,14 @@ $(".core-full-slider").slick({
     {
       breakpoint: 767,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
         slidesToScroll: 1,
       },
     },
