ebf510386dabfcd8791479818979f46468c0cb16
huyhq <huyhq@coccoc.com>
2021-05-05 17:40:40 +0700

    update

--- src/js/module/main.js
+++ src/js/module/main.js
@@ -1,5 +1,9 @@
 import $ from "jquery";
+import Scrollbar from 'smooth-scrollbar';
 import "slick-carousel";
+
+Scrollbar.init(document.querySelector('.wraper'));
+
 /* eslint-disable */
 const tabItem = document.querySelectorAll(".item, .sub-item");
 const tabs = document.querySelectorAll(".tab");
@@ -568,3 +572,26 @@ function intersectionListenerSupported() {
 
     registerEventListeners();
 
+// Parallax effect
+// Adapted from @ilonacodes article ->  https://link.medium.com/7fFiON6Q1X
+
+// Update : added throttle to increase performance
+window.addEventListener('scroll', throttle(parallax, 10));
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+};
+
+function parallax() {
+  var scrolled = window.pageYOffset;
+  var parallax = document.querySelector(".parallax");
+  // You can adjust the 0.4 to change the speed
+	var coords = (scrolled * -0.5) + 'px'
+  parallax.style.transform = 'translateY(' + coords + ')';
+};
